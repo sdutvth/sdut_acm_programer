b336b72f69a911c20767c13d3d158aec47868eb8
sdutvth <vth1311@126.com>
2019-08-19 22:22:32 +0800

    1152

---
+++ 1152.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void solution(){
+    int a,b;
+    a = 123;
+    b = 456;
+    cout<<"sum is "<<a+b<<endl;
+}
+
+int main() {
+    solution();
+    return 0;
+}
